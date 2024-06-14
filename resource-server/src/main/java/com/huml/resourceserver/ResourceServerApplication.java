99bbd95a949be0699db43bd90595f37cf88760c1
zhouziwang.joyboy <zhouziwang.joyboy@bytedance.com>
2024-06-14 14:00:58 +0800

    学习oauth2的流程代码

---
+++ resource-server/src/main/java/com/huml/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,24 @@
+package com.huml.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public Object get(@AuthenticationPrincipal Object user) {
+        return user;
+    }
+
+}
